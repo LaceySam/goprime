decf62bc1e19f5436b75fe4a2724d5adfc59e791
Sam Lacey <sc.lacey90@gmail.com>
2016-10-24 17:56:59 +0100

    C001: Add basic primailty proofing
    - Check candidate is not divisible by first 1k primes
    - Use math.big.ProbablyPrime as secondary test once first has passed

--- prime.go
+++ prime.go
@@ -7,29 +7,43 @@ import (
 )
 
 const (
-	minimumBits = 2048
+	minimumBits          = 2048
+	minimumPrimeAccuracy = 100
 )
 
 type PrimeGenerator struct {
-	Bits            int
-	Source          rand.Source
-	randomGenerator *rand.Rand
+	Bits              int
+	PrimeAccuracy     int
+	Source            rand.Source
+	randomGenerator   *rand.Rand
+	preComputedPrimes []*big.Int
 }
 
-func NewPrimeGenerator(bits int, source rand.Source) *PrimeGenerator {
+func NewPrimeGenerator(bits, primeAccuracy int, source rand.Source) *PrimeGenerator {
 	if bits == 0 || bits < minimumBits {
 		bits = minimumBits
 	}
 
+	if primeAccuracy == 0 || primeAccuracy < minimumPrimeAccuracy {
+		primeAccuracy = minimumPrimeAccuracy
+	}
+
 	if source == nil {
-		// TODO(Sam) Investiagte better randon source?
+		// TODO(Sam): Investiagte better randon source?
 		source = rand.NewSource(time.Now().UnixNano())
 	}
 
+	biggedPreComputedPrimes := []*big.Int{}
+	for _, prime := range preComputedPrimes {
+		biggedPreComputedPrimes = append(biggedPreComputedPrimes, big.NewInt(prime))
+	}
+
 	return &PrimeGenerator{
-		Bits:            bits,
-		Source:          source,
-		randomGenerator: rand.New(source),
+		Bits:              bits,
+		PrimeAccuracy:     primeAccuracy,
+		Source:            source,
+		randomGenerator:   rand.New(source),
+		preComputedPrimes: biggedPreComputedPrimes,
 	}
 }
 
@@ -37,13 +51,12 @@ func (p *PrimeGenerator) GetPrime() (*big.Int, bool) {
 
 	// We're going to need to keep generating random numbers until we prove one is a prime.
 	// Should get a prime within a couple of hundred attempts.
-
 	prime, ok := p.GenerateRandomNumber()
 	if !ok {
 		return &big.Int{}, ok
 	}
 
-	for p.ProvePrime(prime) {
+	for p.ProvePrimality(prime) {
 		prime, ok = p.GenerateRandomNumber()
 		if !ok {
 			return &big.Int{}, ok
@@ -73,6 +86,21 @@ func (p *PrimeGenerator) GenerateRandomNumber() (*big.Int, bool) {
 	return randomNumber.SetString(binarySequence, 2)
 }
 
-func (p *PrimeGenerator) ProvePrime(prime *big.Int) bool {
+func (p *PrimeGenerator) checkAgainstSmallPrimes(candidate *big.Int) bool {
+	for _, prime := range p.preComputedPrimes {
+		mod := new(big.Int)
+		if mod.Mod(candidate, prime) == big.NewInt(0) {
+			return false
+		}
+	}
+
 	return true
+}
+
+func (p *PrimeGenerator) ProvePrimality(candidate *big.Int) bool {
+	if !p.checkAgainstSmallPrimes(candidate) {
+		return false
+	}
+
+	return candidate.ProbablyPrime(p.PrimeAccuracy)
 }
--- prime_test.go
+++ prime_test.go
@@ -6,14 +6,19 @@ import (
 )
 
 var (
-	testP   = NewPrimeGenerator(0, nil)
-	max2047 = new(big.Int)
-	max2048 = new(big.Int)
+	testP            = NewPrimeGenerator(0, 0, nil)
+	max2047          = new(big.Int)
+	max2048          = new(big.Int)
+	knownPrime       = "100010100001111101110100010101011111100001011"
+	knownBigPrime    = new(big.Int)
+	knownNotBigPrime = new(big.Int)
 )
 
 func init() {
 	max2047.Exp(big.NewInt(2), big.NewInt(2047), nil)
 	max2048.Exp(big.NewInt(2), big.NewInt(2048), nil)
+	knownBigPrime.SetString(knownPrime, 2)
+	knownNotBigPrime.Mul(knownBigPrime, big.NewInt(2))
 }
 
 func TestGenerateRandomNumberWithinBitRange(t *testing.T) {
@@ -29,5 +34,19 @@ func TestGenerateRandomNumberWithinBitRange(t *testing.T) {
 	upperBound := n.Cmp(max2048)
 	if upperBound == 1 {
 		t.Fatal("Random number greater than lower bound")
+	}
+}
+
+func TestProvePrimalityWithPrime(t *testing.T) {
+	pass := testP.ProvePrimality(knownBigPrime)
+	if !pass {
+		t.Fatal("Known prime not proved to be prime")
+	}
+}
+
+func TestProvePrimalityWithNotPrime(t *testing.T) {
+	pass := testP.ProvePrimality(knownNotBigPrime)
+	if pass {
+		t.Fatal("Known non-prime proved to be prime")
 	}
 }
